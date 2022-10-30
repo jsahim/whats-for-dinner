85ea027a58bd972332eb3870edaed1d0f02c4399
Jeff Sahim <jeffreysahim@gmail.com>
2022-10-30 18:31:45 -0400

    Add CYOA feature

--- main.js
+++ main.js
@@ -55,17 +55,28 @@ var dessertsIndexNum = getRandomIndex(desserts);
 
 //query selectors
 var recipeName = document.querySelector('#output-name');
-var letsCookButton = document.querySelector('#lets-cook-button')
-var sidesRadioButton = document.querySelector('#side-radio')
-var mainsRadioButton = document.querySelector('#main-radio')
-var dessertsRadioButton = document.querySelector('#dessert-radio')
-var showHiddenContent = document.querySelector('.output-container')
-var showPot = document.querySelector('.pot')
+var letsCookButton = document.getElementById('lets-cook-button');
+var sidesRadioButton = document.querySelector('#side-radio');
+var mainsRadioButton = document.querySelector('#main-radio');
+var dessertsRadioButton = document.querySelector('#dessert-radio');
+var showHiddenContent = document.querySelector('.output-container');
+var showPot = document.querySelector('.pot');
+var loginSubmitButton = document.getElementById('form-submit');
+var usernameField = document.getElementById('form-name');
+var userPlaceholder = document.getElementById('welcome-name');
+var urlExtract = location.search.slice(1).split("=")[1]
 
-//event listeners 
+
+//event listeners
 letsCookButton.addEventListener('click', getRecipe);
+window.addEventListener('load', addName);
+
 
 //functions
+function addName() {
+  userPlaceholder.innerText = urlExtract;
+  console.log("anything")
+  }
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
@@ -101,11 +112,3 @@ function showDessert() {
   showPot.classList.add('hidden');
   recipeName.innerText = desserts[dessertsIndexNum];
 }
-
-
-
-
-
-
-
-    
